854245f7ed99d92ca6891a410998c7efae001ba7
mariaisaeva <misaeva@wakeapp.ru>
2020-09-03 14:55:57 +0700

    CSN-3 added variables

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
+const path = require('path');
+
+function addStyleResource(rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/variables.scss'),
+      ],
+    });
+}
+
 module.exports = {
   runtimeCompiler: true,
+
+  chainWebpack: (config) => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    types.forEach((type) => addStyleResource(config.module.rule('scss').oneOf(type)));
+  },
 };
