f2e403e02fef5844eee606bb915453412054a2b7
mariaisaeva <misaeva@wakeapp.ru>
2020-09-05 02:02:23 +0700

    CSN-3 installed vue-fragment

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import '@/styles/main.scss';
 import Vue from 'vue';
 import VModal from 'vue-js-modal';
+import { Plugin } from 'vue-fragment';
 import App from './App.vue';
 
 Vue.use(VModal);
+Vue.use(Plugin);
 
 new Vue({
   render: (h) => h(App),
