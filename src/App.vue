029fe40e38be1aff381d06fad7989fd25422fbc9
mariaisaeva <misaeva@wakeapp.ru>
2020-09-09 22:48:22 +0700

    CSN-3 added Banner component

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,11 @@
     <Hero />
     <BestGames />
     <Jackpots />
+    <Banner v-bind="banner1" />
     <Tournaments />
     <Promotions />
     <Questions />
-    <BottomBanner />
+    <Banner v-bind="banner2" />
     <Partners />
     <Footer />
   </div>
@@ -21,7 +22,7 @@ import Jackpots from '@/components/Jackpots.vue';
 import Tournaments from '@/components/Tournaments.vue';
 import Promotions from '@/components/Promotions.vue';
 import Questions from '@/components/Questions.vue';
-import BottomBanner from '@/components/BottomBanner.vue';
+import Banner from '@/components/Banner.vue';
 import Partners from '@/components/Partners.vue';
 import Footer from '@/components/Footer.vue';
 
@@ -35,9 +36,28 @@ export default {
     Tournaments,
     Promotions,
     Questions,
-    BottomBanner,
+    Banner,
     Partners,
     Footer,
+  },
+  data() {
+    return {
+      banner1: {
+        image: 'banner1.png',
+        imageMobile: 'banner1_mobile.png',
+        title: 'Live Games in <span class="Colored">Vegaswinner Casino</span>',
+        text: 'Hit the tables and play your cards on the felt in our state-of-the-art live casino. Get all the casino action from the comfort of your own home.',
+        btnText: 'Play now',
+      },
+      banner2: {
+        image: 'banner2.png',
+        imageMobile: 'banner2_mobile.png',
+        title: '50% Second Deposit Bonus',
+        bonus: '€100 + 150 Free spins',
+        text: 'If you’re looking for over 2000 unique game titles with fun features and different payouts, you’ve come to the right place.',
+        btnText: 'Get bonus',
+      },
+    };
   },
 };
 </script>
---
+++ src/components/Banner.vue
@@ -0,0 +1,163 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <div class="Banner">
+    <picture class="Banner-Image">
+      <source media="(max-width: 460px)" :srcset="require(`@/assets/img/${imageMobile}`)">
+      <img :src="require(`@/assets/img/${image}`)" :alt="title">
+    </picture>
+    <div class="Banner-Content">
+      <div v-if="title" class="Title Title--type-h3 Banner-Title">
+        <span v-html="title"></span>
+      </div>
+      <div v-if="bonus" class="Banner-Bonus">
+        {{ bonus }}
+      </div>
+      <div v-if="text" class="Banner-Text">
+        {{ text }}
+      </div>
+      <button v-if="btnText" class="Btn Btn--color Banner-Btn">
+        {{ btnText }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+    imageMobile: {
+      type: String,
+      required: false,
+    },
+    title: {
+      type: String,
+      required: false,
+    },
+    bonus: {
+      type: String,
+      required: false,
+    },
+    text: {
+      type: String,
+      required: false,
+    },
+    btnText: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.Banner {
+  position: relative;
+  margin-bottom: 50px;
+  padding-left: 0;
+  padding-right: 0;
+
+  @media(max-width: $screen-s) {
+    text-align: center;
+  }
+
+  &-Image {
+    object-fit: cover;
+
+    @media(max-width: $screen-s) {
+      display: inline-block;
+      max-height: 380px;
+      overflow: hidden;
+    }
+  }
+
+  &-Content {
+    position: absolute;
+    top: 117px;
+    left: 55px;
+    max-width: 40%;
+
+    @media(max-width: $screen-l) {
+      max-width: 50%;
+    }
+
+    @media(max-width: $screen-s) {
+      position: relative;
+      top: initial;
+      left: initial;
+      max-width: 100%;
+      margin-top: -120px;
+      padding-left: 16px;
+      padding-right: 16px;
+    }
+  }
+
+  &-Title {
+    margin-bottom: 42px;
+    font-size: 36px;
+
+    @media(max-width: $screen-l) {
+      margin-bottom: 28px;
+      font-size: 26px;
+    }
+
+    @media(max-width: $screen-m) {
+      margin-bottom: 24px;
+    }
+
+    @media(max-width: $screen-s) {
+      margin-bottom: 18px;
+      font-size: 22px;
+    }
+  }
+
+  &-Bonus {
+    margin-bottom: 22px;
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 126.5%;
+    text-transform: uppercase;
+    color: var(--color-main1);
+
+    @media(max-width: $screen-l) {
+      margin-bottom: 13px;
+      font-size: 20px;
+    }
+
+    @media(max-width: $screen-s) {
+      margin-bottom: 20px;
+      font-size: 14px;
+    }
+  }
+
+  &-Text {
+    margin-bottom: 34px;
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 24px;
+    color: var(--color-text-ghost);
+
+    @media(max-width: $screen-m) {
+      font-size: 14px;
+    }
+
+    @media(max-width: $screen-m) {
+      margin-bottom: 23px;
+      font-size: 13px;
+      color: var(--color-text-main);
+    }
+  }
+
+  &-Btn {
+    padding: 20px 32px;
+
+    @media(max-width: $screen-m) {
+      padding: 18px 22px;
+    }
+  }
+}
+</style>
