e1da48de7308c3945162444d3185a49c11c7107a
mariaisaeva <misaeva@wakeapp.ru>
2020-09-17 17:28:00 +0700

    CSN-3 added marquee text

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,44 +1,49 @@
 <template>
   <section class="Partners">
-    <div class="Partners-Partner Partners-Partner--visa">
-      <img class="Partners-Image" src="@/assets/img/visa.svg" />
-    </div>
-    <div class="Partners-Partner Partners-Partner--maestro">
-      <img class="Partners-Image" src="@/assets/img/maestro.svg" />
-    </div>
-    <div class="Partners-Partner">
-      <img class="Partners-Image" src="@/assets/img/skrill.svg" />
-    </div>
-    <div class="Partners-Partner Partners-Partner--paysafe">
-      <img class="Partners-Image" src="@/assets/img/paysafe.svg" />
-    </div>
-    <div class="Partners-Partner">
-      <img class="Partners-Image" src="@/assets/img/wirecard.svg" />
-    </div>
-    <div class="Partners-Partner">
-      <img class="Partners-Image" src="@/assets/img/webmoney.svg" />
-    </div>
-    <div class="Partners-Partner Partners-Partner--sofort">
-      <img class="Partners-Image" src="@/assets/img/sofort.svg" />
-    </div>
-    <div class="Partners-Partner">
-      <img class="Partners-Image" src="@/assets/img/microgaming.svg" />
-    </div>
+    <marquee-text :duration="60">
+      <div class="Partners-Wrapper">
+        <div class="Partners-Partner Partners-Partner--visa">
+          <img class="Partners-Image" src="@/assets/img/visa.svg" />
+        </div>
+        <div class="Partners-Partner Partners-Partner--maestro">
+          <img class="Partners-Image" src="@/assets/img/maestro.svg" />
+        </div>
+        <div class="Partners-Partner">
+          <img class="Partners-Image" src="@/assets/img/skrill.svg" />
+        </div>
+        <div class="Partners-Partner Partners-Partner--paysafe">
+          <img class="Partners-Image" src="@/assets/img/paysafe.svg" />
+        </div>
+        <div class="Partners-Partner">
+          <img class="Partners-Image" src="@/assets/img/wirecard.svg" />
+        </div>
+        <div class="Partners-Partner">
+          <img class="Partners-Image" src="@/assets/img/webmoney.svg" />
+        </div>
+        <div class="Partners-Partner Partners-Partner--sofort">
+          <img class="Partners-Image" src="@/assets/img/sofort.svg" />
+        </div>
+        <div class="Partners-Partner">
+          <img class="Partners-Image" src="@/assets/img/microgaming.svg" />
+        </div>
+      </div>
+    </marquee-text>
   </section>
 </template>
 
 <script>
+import MarqueeText from 'vue-marquee-text-component';
+
 export default {
   name: 'Partners',
+  components: {
+    MarqueeText,
+  },
 };
 </script>
 
 <style lang="scss">
 .Partners {
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: space-between;
-  align-items: center;
   padding-bottom: 40px;
   border-bottom: 1px solid rgba(110, 88, 131, 0.3);
 
@@ -48,6 +53,13 @@ export default {
 
   @media(max-width: $screen-m) {
     padding-bottom: 20px;
+  }
+
+  &-Wrapper {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    align-items: center;
   }
 
   &-Partner {
@@ -65,7 +77,6 @@ export default {
     }
 
     @media(max-width: $screen-s) {
-      display: none;
       margin-right: 10px;
       margin-bottom: 10px;
 
