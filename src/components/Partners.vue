0490176f975c977cc87df4745a30c8f25af0ec97
mariaisaeva <misaeva@wakeapp.ru>
2020-10-19 21:09:25 +0300

    CSN-3 replaced partners

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -3,28 +3,46 @@
     <marquee-text :duration="60">
       <div class="Partners-Wrapper">
         <div class="Partners-Partner">
-          <img class="Partners-Image" src="@/assets/img/visa.svg" />
+          <img loading="lazy" class="Partners-Image" src="https://service.image-tech-storage.com/payments/e650ceed-000c-485f-9291-1f758a6523a4_medium_light.png" />
         </div>
         <div class="Partners-Partner">
-          <img class="Partners-Image" src="@/assets/img/maestro.svg" />
+          <img loading="lazy" class="Partners-Image" src="https://service.image-tech-storage.com/payments/6d5fa71f-43b5-42fc-a39e-e62fe04b7d0e_tiny_light.png" />
         </div>
         <div class="Partners-Partner">
-          <img class="Partners-Image" src="@/assets/img/skrill.svg" />
+          <img loading="lazy" class="Partners-Image" src="https://service.image-tech-storage.com/payments/790dc286-044f-478f-9b8d-be53bd925bad_tiny_light.png" />
         </div>
         <div class="Partners-Partner">
-          <img class="Partners-Image" src="@/assets/img/paysafe.svg" />
+          <img loading="lazy" class="Partners-Image" src="https://service.image-tech-storage.com/payments/dd15ead8-9192-4ac7-88c5-e99e240a8ebe_tiny_light.png" />
         </div>
         <div class="Partners-Partner">
-          <img class="Partners-Image" src="@/assets/img/wirecard.svg" />
+          <img loading="lazy" class="Partners-Image" src="https://service.image-tech-storage.com/payments/a5fc4d09-5618-454b-88d2-34aef3923c70_tiny_light.png" />
         </div>
         <div class="Partners-Partner">
-          <img class="Partners-Image" src="@/assets/img/webmoney.svg" />
+          <img loading="lazy" class="Partners-Image" src="https://service.image-tech-storage.com/payments/06d7c0a6-007e-405d-a384-272e80e62811_tiny_light.png" />
         </div>
         <div class="Partners-Partner">
-          <img class="Partners-Image" src="@/assets/img/sofort.svg" />
+          <img loading="lazy" class="Partners-Image" src="https://service.image-tech-storage.com/payments/6c40c85b-1ac9-4dde-9020-13b876bb16e9_tiny_light.png" />
         </div>
         <div class="Partners-Partner">
-          <img class="Partners-Image" src="@/assets/img/microgaming.svg" />
+          <img loading="lazy" class="Partners-Image" src="https://service.image-tech-storage.com/payments/97e8d550-9020-4d12-98ff-e0c17e45124a_tiny_light.png" />
+        </div>
+        <div class="Partners-Partner">
+          <img loading="lazy" class="Partners-Image" src="@/assets/img/maestro.svg" />
+        </div>
+        <div class="Partners-Partner">
+          <img loading="lazy" class="Partners-Image" src="https://service.image-tech-storage.com/payments/591b6ba5-04ff-41ef-904a-1722f0a6aa0c_tiny_light.png" />
+        </div>
+        <div class="Partners-Partner">
+          <img loading="lazy" class="Partners-Image" src="https://service.image-tech-storage.com/payments/d1442073-eaf5-49f1-aaf5-d600edf436fb_tiny_light.png" />
+        </div>
+        <div class="Partners-Partner">
+          <img loading="lazy" class="Partners-Image" src="https://service.image-tech-storage.com/payments/33f94f44-cfbb-4d77-bb69-26c9dadd4b6b_tiny_light.png" />
+        </div>
+        <div class="Partners-Partner">
+          <img loading="lazy" class="Partners-Image" src="@/assets/img/microgaming.svg" />
+        </div>
+        <div class="Partners-Partner">
+          <img loading="lazy" class="Partners-Image" src="@/assets/img/evolution_gaming.svg" />
         </div>
       </div>
     </marquee-text>
@@ -68,17 +86,9 @@ export default {
     margin-bottom: 10px;
     opacity: 0.25;
 
-    &:last-child {
-      margin-right: 0;
-    }
-
     @media(min-width: $screen-m) {
       margin-right: 25px;
       margin-bottom: 15px;
-    }
-
-    @media(min-width: $screen-xl) {
-      margin-right: 37px;
     }
   }
 }
